d1a9c1a8ad2b665feb4d105a1c22045fe48857b6
benjamindenise <benjamin.denise@protonmail.com>
2022-03-01 15:52:19 +0100

    update merging method

--- DataObservable/merge.py
+++ DataObservable/merge.py
@@ -5,29 +5,44 @@ import csv
 if __name__ == '__main__':
     db_name = input("Nom de la BDD à ajouter : ")
     maxAnalyseId = 0
+    maxSwimmerId = 0
+    matchSwimmerId = {}
     fields = {}
 
     # Table analyse
 
-    with open('all/analyses.csv', 'r', newline='', encoding='UTF8') as all_analyse:
+    with open('all/analyses.csv', 'r', newline='', encoding='utf-8-sig') as all_analyse:
         analyses_reader = csv.DictReader(all_analyse, delimiter=';')
         for analyse in analyses_reader:
+            # On met à jours le max des identifiants d'analyse
             if(int(analyse['analyses.id']) > maxAnalyseId) :
                 maxAnalyseId = int(analyse['analyses.id'])
+            # On met à jours le max des identifiants des nageurs
+            if(int(analyse['nageurs.id']) > maxAnalyseId) :
+                maxSwimmerId = int(analyse['nageurs.id'])
+            # On associe un nom à un identifiant
+            matchSwimmerId[analyse['prenom']+analyse['nageurs.nom']] = int(analyse['nageurs.id'])
         fields = analyses_reader.fieldnames
 
     i = 1
+    swimId = 1
     match = {}      # Correspondance entre ancien id et nouveau
 
-    with open('all/analyses.csv', 'a', newline='', encoding='UTF8') as all_analyse:
+    with open('all/analyses.csv', 'a', newline='', encoding='utf-8-sig') as all_analyse:
         analyses_writter = csv.DictWriter(all_analyse, delimiter=';', fieldnames= fields)
 
-        with open(db_name+'/analyses.csv', 'r', newline='', encoding='UTF8') as new_analyse :
+        with open(db_name+'/analyses.csv', 'r', newline='', encoding='utf-8-sig') as new_analyse :
             new_analyses_reader = csv.DictReader(new_analyse, delimiter=';', fieldnames=fields)
             for analyse in list(new_analyses_reader)[1::] :
                 new_analyse = analyse
                 match[int(analyse['analyses.id'])] = maxAnalyseId + i
                 new_analyse['analyses.id'] = maxAnalyseId + i
+                if analyse['prenom']+analyse['nageurs.nom'] in matchSwimmerId :
+                    new_analyse['nageurs.id'] = matchSwimmerId[analyse['prenom']+analyse['nageurs.nom']]
+                else :
+                    new_analyse['nageurs.id'] = maxSwimmerId + swimId
+                    matchSwimmerId[analyse['prenom']+analyse['nageurs.nom']] = maxSwimmerId + swimId
+                    swimId += 1
                 analyses_writter.writerow(new_analyse)
                 i +=1
 
@@ -35,7 +50,7 @@ if __name__ == '__main__':
     maxId = 0
     fileds = {}
 
-    with open('all/longueurs.csv', 'r', newline='', encoding='UTF8') as all:
+    with open('all/longueurs.csv', 'r', newline='', encoding='utf-8-sig') as all:
         reader = csv.DictReader(all, delimiter=';')
         for elt in reader:
             if(int(elt['id']) > maxId) :
@@ -44,10 +59,10 @@ if __name__ == '__main__':
 
     i = 1
 
-    with open('all/longueurs.csv', 'a', newline='', encoding='UTF8') as all:
+    with open('all/longueurs.csv', 'a', newline='', encoding='utf-8-sig') as all:
         writter = csv.DictWriter(all, delimiter=';', fieldnames= fields)
 
-        with open(db_name+'/longueurs.csv', 'r', newline='', encoding='UTF8') as new :
+        with open(db_name+'/longueurs.csv', 'r', newline='', encoding='utf-8-sig') as new :
             new_reader = csv.DictReader(new, delimiter=';', fieldnames=fields)
             for elt in list(new_reader)[1::] :
                 new_elt = elt
@@ -60,7 +75,7 @@ if __name__ == '__main__':
     maxId = 0
     fileds = {}
 
-    with open('all/mouvements.csv', 'r', newline='', encoding='UTF8') as all:
+    with open('all/mouvements.csv', 'r', newline='', encoding='utf-8-sig') as all:
         reader = csv.DictReader(all, delimiter=';')
         for elt in reader:
             if(int(elt['id']) > maxId) :
@@ -69,10 +84,10 @@ if __name__ == '__main__':
 
     i = 1
 
-    with open('all/mouvements.csv', 'a', newline='', encoding='UTF8') as all:
+    with open('all/mouvements.csv', 'a', newline='', encoding='utf-8-sig') as all:
         writter = csv.DictWriter(all, delimiter=';', fieldnames= fields)
 
-        with open(db_name+'/mouvements.csv', 'r', newline='', encoding='UTF8') as new :
+        with open(db_name+'/mouvements.csv', 'r', newline='', encoding='utf-8-sig') as new :
             new_reader = csv.DictReader(new, delimiter=';', fieldnames=fields)
             for elt in list(new_reader)[1::] :
                 new_elt = elt
@@ -85,7 +100,7 @@ if __name__ == '__main__':
     maxId = 0
     fileds = {}
 
-    with open('all/sections.csv', 'r', newline='', encoding='UTF8') as all:
+    with open('all/sections.csv', 'r', newline='', encoding='utf-8-sig') as all:
         reader = csv.DictReader(all, delimiter=';')
         for elt in reader:
             if(int(elt['id']) > maxId) :
@@ -94,10 +109,10 @@ if __name__ == '__main__':
 
     i = 1
 
-    with open('all/sections.csv', 'a', newline='', encoding='UTF8') as all:
+    with open('all/sections.csv', 'a', newline='', encoding='utf-8-sig') as all:
         writter = csv.DictWriter(all, delimiter=';', fieldnames= fields)
 
-        with open(db_name+'/sections.csv', 'r', newline='', encoding='UTF8') as new :
+        with open(db_name+'/sections.csv', 'r', newline='', encoding='utf-8-sig') as new :
             new_reader = csv.DictReader(new, delimiter=';', fieldnames=fields)
             for elt in list(new_reader)[1::] :
                 new_elt = elt
